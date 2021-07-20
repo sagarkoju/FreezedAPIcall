6f1169334e9f46e1400b216feec9c13c332a6051
sagarkoju <sagar@fawesomeapps.com>
2021-07-20 17:57:15 +0545

    tabbar apu updated

--- lib/model/dataapi.dart
+++ lib/model/dataapi.dart
@@ -1,30 +1,57 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<Food> fetchdata() async {
-  final response = await http.get(Uri.parse(
-      'http://backend.quickbites.delivery/api/restaurant_food_list/94?fbclid=IwAR17F2cHG0aMJkuZPuomVdGMfKh_tf6sKatz0i3OkPdpgFdzGASPlIJCucg'));
+// Future<Food> fetchdata() async {
+//   final response = await http.get(Uri.parse(
+//       'http://backend.quickbites.delivery/api/restaurant_food_list/94?fbclid=IwAR17F2cHG0aMJkuZPuomVdGMfKh_tf6sKatz0i3OkPdpgFdzGASPlIJCucg'));
 
-  if (response.statusCode == 200) {
-    return Food.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to load album');
+//     List<Food> list = [];
+
+//     try {
+//       if (response.statusCode == 200) {
+//         Map<String, dynamic> map = json.decode(response.body);
+
+//         for (var map in map['foodlists']) {
+//           list.add(Food(id: map['id'], name: map['name'], foodItems: map['fooditem']));
+//         }
+//       }
+//     } catch (e, _) {
+//       debugPrint(e.toString());
+//     }
+//     return fetchdata();
+
+// }
+class ApiService {
+  List<Food> getData = [];
+
+  Future<List<Food>> getDataModel() async {
+    final res = await http.get(
+      Uri.parse(
+          "https://newweb.nepalstock.com/api/nots/securityDailyTradeStat/58"),
+      headers: {
+        'user-agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      },
+    );
+
+    getData = List.from(jsonDecode(res.body).map((e) => Food.fromJson(e)));
+    return getData;
   }
 }
 
 class Food {
   final int id;
-  final List foodItems;
+
   final String name;
 
   factory Food.fromJson(Map<String, dynamic> json) {
     return Food(
-      foodItems: json['foodItems'],
       id: json['id'],
       name: json['name'],
     );
   }
 
-  Food({required this.id, required this.foodItems, required this.name});
+  Food({required this.id, required this.name});
 }
--- lib/page/silverappbar.dart
+++ lib/page/silverappbar.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:demo/model/dataapi.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class SilverAppBarScreen extends StatefulWidget {
   @override
@@ -8,85 +13,86 @@ class SilverAppBarScreen extends StatefulWidget {
 
 class _SilverAppBarWithTabBarState extends State<SilverAppBarScreen>
     with SingleTickerProviderStateMixin {
+  List food = [];
+  List foodlist = [];
+
+  getCountries() async {
+    final response = await http.get(
+      Uri.parse(
+          'http://backend.quickbites.delivery/api/restaurant_food_list/94?fbclid=IwAR17F2cHG0aMJkuZPuomVdGMfKh_tf6sKatz0i3OkPdpgFdzGASPlIJCucg'),
+      headers: {
+        'user-agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      },
+    );
+    Map<String, dynamic> data =
+        new Map<String, dynamic>.from(json.decode(response.body));
+    return data;
+  }
+
   TabController? controller;
   List<Widget> tabs = [
     TabViewList(title: 'Tab1'),
     TabViewList(title: 'Tab2'),
     TabViewList(title: 'Tab3'),
   ];
-  Future<Food>? foodlist;
+
   @override
   void initState() {
     super.initState();
-    foodlist = fetchdata();
-    controller =
-        new TabController(length: foodlist!.toString().length, vsync: this);
+    getCountries().then((data) {
+      setState(() {
+        foodlist = food;
+      });
+    });
+    controller = new TabController(length: foodlist.length, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: foodlist,
-      builder: (c, s) {
-        if (s.hasData) {
-          List<Tab> tabs = <Tab>[];
-
-          for (int i = 0; i < s.data.toString().length; i++) {
-            tabs.add(Tab(
-              child: Text(
-                 s.data.foodlist[i].name,
-                style: TextStyle(color: Colors.white),
-              ),
-            ));
-          }
-          
-        }
-         return DefaultTabController(
-            length: foodlist.toString().length,
-            child: Scaffold(
-              body: NestedScrollView(
-                physics: BouncingScrollPhysics(),
-                headerSliverBuilder:
-                    (BuildContext context, bool innerBoxIsScrolled) {
-                  return <Widget>[
-                    SliverOverlapAbsorber(
-                      handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
-                          context),
-                      sliver: SliverSafeArea(
-                        top: false,
-                        sliver: SliverAppBar(
-                          expandedHeight: 200.0,
-                          floating: false,
-                          pinned: true,
-                          flexibleSpace: FlexibleSpaceBar(
-                              centerTitle: true,
-                              title: Text("Collapsing Toolbar",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16.0,
-                                  )),
-                              background: Image.network(
-                                "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
-                                fit: BoxFit.cover,
-                              )),
-                        ),
-                      ),
-                    ),
-                    //
-                  ];
-                },
-                body: Container(
-                  child: TabBar(
-                      controller: controller,
-                      labelColor: Colors.black87,
-                      unselectedLabelColor: Colors.grey,
-                      tabs: tabs),
+    return Scaffold(
+      body: NestedScrollView(
+        physics: BouncingScrollPhysics(),
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverOverlapAbsorber(
+              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+              sliver: SliverSafeArea(
+                top: false,
+                sliver: SliverAppBar(
+                  expandedHeight: 400.0,
+                  floating: false,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                      centerTitle: true,
+                      title: Text("Collapsing Toolbar",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          )),
+                      background: Image.network(
+                        "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+                        fit: BoxFit.cover,
+                      )),
                 ),
               ),
             ),
-          
-        );
-      },
+//
+          ];
+        },
+        body: Container(
+          child: ListView.builder(
+              itemCount: foodlist.length,
+              itemBuilder: (BuildContext context, int index) {
+                return TabBar(
+                    controller: controller,
+                    labelColor: Colors.black87,
+                    unselectedLabelColor: Colors.grey,
+                     tabs: [],
+                    );
+              }),
+        ),
+      ),
     );
   }
 }
